1d04e3a543e828f0d9e8b09dd8bb10f4db302ff7
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2023-03-28 22:42:15 +0300

    update bot

---
+++ src/configs/config.ts
@@ -0,0 +1,7 @@
+import "dotenv/config";
+
+export const configs = {  
+    BOT_TOKEN: process.env.BOT_TOKEN!,
+    WHITELISTED_USERS: [5688202073, 1195869296],
+    INTERVAL: 15
+}
--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,62 @@
 import puppeteer from "puppeteer";
 import * as fs from "fs"
-import axios from "axios"
-import FormData from "form-data";
-import dotenv from "dotenv"
-import express from "express"
+import { Context, Telegraf } from "telegraf";
+import { configs } from "./configs";
 
-const url:string = "https://cryptobubbles.net/"
+let bot: Telegraf;
+const url = 'https://cryptobubbles.net/';
 
-const app = express()
-
-
-const interval:number = 15;
-const bot_token = process.env.BOT_TOKEN
-const telegramEndpoint = `https://api.telegram.org/bot${bot_token}/sendPhoto`
-const chatId = process.env.CHAT_ID
-
-async function takeScreenshot(): Promise<void> {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(`${url}?time=hour`);
-    await page.waitForTimeout(30000);
-    const screenshot = await page.screenshot();
-    await browser.close();
-    const filename = `screenshot-${Date.now()}.png`;
-    fs.writeFileSync(filename, screenshot);
-    const formData = new FormData({ maxDataSize: 64 * 1024 });
-    formData.append('chat_id', chatId);
-    formData.append('photo', fs.createReadStream(filename));
-    await axios.post(telegramEndpoint, formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
+bot = new Telegraf(configs.BOT_TOKEN);
+export const messageHome = async (message: string, ctx: Context) => {
+  return ctx.replyWithMarkdownV2(message);
+};
+bot.use(async (ctx: any, next: any) => {
+  try {
+    if (ctx.message?.from) {
+      const params: any = ctx.message?.from;
+      const { id, first_name, last_name, username } = params;
+      if (!configs.WHITELISTED_USERS.includes(id)) {
+        return ctx.reply(
+          `Hello ${
+            first_name || last_name || username
+          }, You are not whitelisted to receive notifications`
+        );
       }
-    });
-    fs.unlinkSync(filename);
+    }
+    return next();
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+bot.start(async (ctx: any) => {
+  const defaultMessage =
+    `Hello Welcome to ${ctx?.me}, a screen short posting bot🔥`.replaceAll(
+      "_",
+      "\\_"
+    );
+  await messageHome(defaultMessage, ctx);
+  setInterval(takeScreenshot, configs.INTERVAL * 60 * 1000);
+});
+
+bot.launch()
+
+
+// Function that takes a screenshot of the website, sets the default tab to "hour", waits for 30 seconds after loading the page, saves the screenshot to a file, and sends it to the Telegram bot
+async function takeScreenshot(): Promise<void> {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  // Set the default tab to "hour" by appending the "?time=hour" query parameter to the URL
+  await page.goto(`${url}?time=hour`);
+  await page.waitForTimeout(30000); // wait for 30 seconds before taking the screenshot
+  const screenshot = await page.screenshot();
+  console.log('screenshot taken')
+  await browser.close();
+  const filename = `screenshot-${Date.now()}.png`;
+  fs.writeFileSync(filename, screenshot);
+  for (const chatId of configs.WHITELISTED_USERS) {
+    await bot.telegram.sendPhoto(chatId, { source: filename });
+  }
+  fs.unlinkSync(filename); // delete the screenshot file after sending it to the Telegram bot
 }
-
-setInterval(takeScreenshot, interval * 60 * 1000)
-
-app.listen(3000, () => {
-    console.log("server is runing on port 3000")
-})
